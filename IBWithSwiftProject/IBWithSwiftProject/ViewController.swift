ed175998ca1f19192e45ae1f78aa84a3edef1ccd
github username <github email address>
2016-07-06 18:20:31 -0400

    lab complete

---
+++ IBWithSwiftProject/IBWithSwiftProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IBWithSwiftProject
+//
+//  Created by Cenker Demir on 7/6/16.
+//  Copyright © 2016 Cenker Demir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
